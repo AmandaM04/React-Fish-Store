b0991ced1247907ab9e499bed8a03d25ffffb7f6
Amanda Mitchell <wreedamitchell@gmail.com>
2018-07-09 19:25:37 -0500

    inventory showing up

--- src/components/Inventory/Inventory.js
+++ src/components/Inventory/Inventory.js
@@ -1,11 +1,37 @@
 import React from 'react';
 
+import fishRequests from '../../firebaseRequest/fishes';
+
 import './Inventory.css';
 
 class Inventory extends React.Component {
+  state = {
+    fishes: [],
+  }
+
+  componentDidMount () {
+    fishRequests
+      .getRequest()
+      .then((fishes) => {
+        this.setState({ fishes });
+      })
+      .catch((error) => {
+        console.error(error.message);
+      });
+  }
   render () {
+    const fishComponents = this.state.fishes.map((fish) => {
+      return (
+        <h2>{fish.name}</h2>
+      );
+    });
     return (
-      <h1 className="Inventory">Inventory</h1>
+      <div className="Inventory">
+        <h1>Inventory</h1>
+        <ul className="fishes">
+          {fishComponents}
+        </ul>
+      </div>
     );
   }
 }
